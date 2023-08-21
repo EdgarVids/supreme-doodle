33b2b266cfcaa5dd1deced0888daaff67fcd0160
EdgarVids <112506357+EdgarVids@users.noreply.github.com>
2023-08-21 22:44:58 +0100

    import env variable with token

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+//import "net/http"
+import "os"
 
 func main() {
+    token := os.Getenv("SPACETRADERS")
     fmt.Println("Space Traders Client Practice")
+    fmt.Println("Token:", token)
+
+
 }
